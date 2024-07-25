06bd7c8a85f6668fcea5b5b3c2201c196631b512
JJA <jericjohnaligato@gmail.com>
2024-07-26 00:47:15 +0800

    another annoying comment

--- frontend/src/js/pages/RecipePage.vue
+++ frontend/src/js/pages/RecipePage.vue
@@ -1,8 +1,29 @@
 <template>
-    <MainLayout>
-        <div class="h-screen">Welcome to Recipe Page 🍪💞</div>
-    </MainLayout>
+  <MainLayout>
+    <div class="h-screen">Welcome to Recipe Page 🍪💞</div>
+    <div v-if="token">
+      {{ user.name }}
+      {{ user.email }}
+    </div>
+  </MainLayout>
 </template>
 <script setup>
-    import MainLayout from "@/layout/MainLayout.vue";
+import MainLayout from '@/layout/MainLayout.vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const token = localStorage.getItem('token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+const user = ref('')
+const loadUser = async () => {
+  const response = await axios.get('/api/user/profile')
+  user.value = response.data
+}
+
+onMounted(() => {
+  loadUser()
+})
 </script>
