d6694793253edb758ea91f372862eaad9d0ee059
ianman <ianfredcaballero@gmail.com>
2024-07-26 00:14:29 +0800

    created banner

--- frontend/src/js/components/CalculatorForm.vue
+++ frontend/src/js/components/CalculatorForm.vue
@@ -1,40 +1,44 @@
 <template>
     <div class="flex">
-    <v-card color="#ebf2f3" style="width: 50%;">
-        <v-card-title>Recipe Name</v-card-title>
-        <v-textarea
-            label="Name"
-            row-height="15"
-            rows="1"
-            variant="outlined"
-            auto-grow="auto-grow"
-            style="width: 95%; padding-left: 1rem;"></v-textarea>
-        <v-card-title>Recipe Ingredients</v-card-title>
+    <v-card color="#ebf2f3" style="width: 55%;">
+      <v-card-title>Select Recipe</v-card-title>
+      <v-select
+        v-model="selectedRecipeId"
+        :items="recipes"
+        item-text="name"
+        item-value="id"
+        label="Select"
+        @change="loadIngredients"
+        style="width: 95%; padding-left: 1rem;"
+      ></v-select>
+      
         <v-textarea
             label="Ingredients"
             variant="outlined"
             auto-grow="auto-grow"
             style="width: 95%; padding-left: 1rem;"></v-textarea>
 
-        <v-row>
-            <v-col> 
-                <v-textarea style="width: 90%; padding-left: 1rem;"
-                    label="No. of Serving(s)"
-                                row-height="15"
-            rows="1"
-                    variant="outlined"
-                    auto-grow="auto-grow"></v-textarea>
-            </v-col>
+            <v-row>
             <v-col>
-                <v-textarea style="width: 90%; padding-left: 1rem;"
-                    label="Serving Size"
-            row-height="15"
-            rows="1"
-                    variant="outlined"
-                    auto-grow="auto-grow"></v-textarea>
-            </v-col>
-        </v-row>
-        <v-col cols="auto">
+          <v-text-field
+            v-model.number="numServings"
+            label="No. of Serving(s)"
+            type="number"
+            variant="outlined"
+            style="width: 90%; padding-left: 1rem;"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model.number="servingSize"
+            label="Serving Size"
+            type="number"
+            variant="outlined"
+            style="width: 90%; padding-left: 1rem;"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-col cols="auto">
             <v-btn color="#024A42" size="x-large">Calculate Recipe</v-btn>
         </v-col>
     </v-card>
@@ -44,16 +48,47 @@
         </v-card>
     </v-col>
     <v-col cols="6">
-      <v-container class="bg-surface-variant">
-        </v-container>
+      <!-- <v-container class="bg-surface-variant">
+        </v-container> -->
     </v-col>
     </v-row>
     </div>
 </template>
 
-<script>
-export default{
-    name: 'CalculatorForm'
-}
-</script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+const recipes = ref([]);
+const selectedRecipeId = ref(null);
+const ingredients = ref([]);
+const numServings = ref(0);
+const servingSize = ref(0);
+const nutritionValues = ref(null);
+
+const loadRecipes = async () => {
+  const response = await axios.get('/api/recipes');
+  recipes.value = response.data;
+};
+
+const loadIngredients = async () => {
+  if (selectedRecipeId.value) {
+    const response = await axios.get(`/api/recipes/${selectedRecipeId.value}/ingredients`);
+    ingredients.value = response.data;
+  }
+};
+
+const calculateNutrition = async () => {
+  const response = await axios.post('/api/calculate-nutrition', {
+    recipe_id: selectedRecipeId.value,
+    num_servings: numServings.value,
+    serving_size: servingSize.value
+  });
+  nutritionValues.value = response.data;
+};
+
+onMounted(() => {
+  loadRecipes();
+});
+
+</script>
