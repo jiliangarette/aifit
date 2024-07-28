fa64f87f694dce434e1cbfc21f609c7cf59e5458
Jilian Garette A. Abangan <92016876+Jgarette0@users.noreply.github.com>
2024-07-28 21:07:38 +0800

    done recipe page  with nutrition facts page (#26)
    recipe page done. 
    nested routing 
    /recipe
    /recipe/meal-plan
    /recipe/favorite
    /recipe/favorite/nutrition-facts/id

--- frontend/src/pages/CommunityPage.vue
+++ frontend/src/pages/CommunityPage.vue
@@ -1,16 +1,20 @@
 <template>
     <MainLayout>
-        <div class="h-screen">
-            <router-link v-for="destination in destinations.recipe_" :key="destination.id" :to="`/${destination.slug}`">{{
-                destination.name }}
-                <img :src="`/images/${destination.image}`" :alt="destination.name"></router-link>
+        <div class="">
+            <!-- Loop through the first recipe's ingredients -->
+            <div v-for="(ingredientName, index) in recipe_ingredients" :key="index">
+                {{ ingredientName.ingredient.name }}
+            </div>
         </div>
     </MainLayout>
 </template>
+
 <script setup>
     import MainLayout from "@/layout/MainLayout.vue";
     import sourceData from '@/data.json';
     import { ref } from "vue";
 
-    const destinations = ref(sourceData.recipes)
+    // Assuming sourceData.recipes is an array and we are using the first recipe
+    const recipe = ref(sourceData.recipes[1]); // Get the first recipe
+    const recipe_ingredients = ref(recipe.value.recipe_ingredients); // Access its ingredients
 </script>
