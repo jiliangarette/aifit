d786775eabe8db6af4eaebc97b569023e1d66968
JJA <jericjohnaligato@gmail.com>
2024-07-28 17:48:09 +0800

    added health benefits to database and made createRecipeForm.vue for inserting recipes nya ge import nako siya sa recipepage.vue

--- frontend/src/pages/RecipePage.vue
+++ frontend/src/pages/RecipePage.vue
@@ -3,10 +3,12 @@
     <RecipeTopSection />
     <router-view />
     <RecipeCallToAction />
+    <createRecipeForm /> 
     <RecipeDescription />
   </MainLayout>
 </template>
 <script setup>
+  import createRecipeForm from "../components/createRecipeForm.vue";
   import MainLayout from "@/layout/MainLayout.vue";
   import RecipeTopSection from "@/components/RecipeTopSection.vue";
   import RecipeCallToAction from "../components/RecipeCallToAction.vue";
